71ef83cc61dd548e32c1d24d19689b4d46218ffb
Teletha <Teletha.T@gmail.com>
2021-05-13 13:00:04 +0900

    progress...

--- src/main/java/stylist/property/Background.java
+++ src/main/java/stylist/property/Background.java
@@ -9,7 +9,7 @@
  */
 package stylist.property;
 
-import static stylist.value.Color.*;
+import static stylist.value.Color.Transparent;
 import static stylist.value.Unit.*;
 
 import java.awt.image.BufferedImage;
@@ -527,28 +527,10 @@ public class Background extends PropertyDefinition<Background> implements ColorH
         }
 
         /**
-         * <pre>
-        background-image:
-        radial-gradient(circle at 29% 55%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 4%, transparent 4%, transparent 44%, transparent 44%, transparent 100%),
-        radial-gradient(circle at 85% 89%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 51%, transparent 51%, transparent 52%, transparent 52%, transparent 100%),
-        radial-gradient(circle at 6% 90%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 53%, transparent 53%, transparent 64%, transparent 64%, transparent 100%),
-        radial-gradient(circle at 35% 75%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 6%, transparent 6%, transparent 98%, transparent 98%, transparent 100%),
-        radial-gradient(circle at 56% 75%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 16%, transparent 16%, transparent 23%, transparent 23%, transparent 100%),
-        radial-gradient(circle at 42% 0%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 3%, transparent 3%, transparent 26%, transparent 26%, transparent 100%),
-        radial-gradient(circle at 29% 28%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 51%, transparent 51%, transparent 75%, transparent 75%, transparent 100%),
-        radial-gradient(circle at 77% 21%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 35%, transparent 35%, transparent 55%, transparent 55%, transparent 100%),
-        radial-gradient(circle at 65% 91%, hsla(329, 0%, 99%, 0.05) 0%, hsla(329, 0%, 99%, 0.05) 46%, transparent 46%, transparent 76%, transparent 76%, transparent 100%),
-        linear-gradient(45deg, rgb(83, 91, 235), rgb(76, 11, 174));
-        </pre>
+         * Draw css image.
          * 
-         * @param one
-         * @param two
-         * @param three
          * @return
          */
-        public static BackgroundImage[] drawCircleGradients(Color one, Color two, Color three) {
-        }
-
         public static BackgroundImage[] drawSubtleTexture(Color base, Color line) {
             return BackgroundImage.of(new LinearGradient().angle(45, deg)
                     .color(line, 0, px)
