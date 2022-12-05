6b9884a28805d6a0f1994fb577834db33b1bd79b
Teletha <Teletha.T@gmail.com>
2022-12-05 19:24:35 +0900

    fix: ensure minimum java version

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -1,3 +1,5 @@
+import javax.lang.model.SourceVersion;
+
 /*
  * Copyright (C) 2021 stylist Development Team
  *
@@ -11,6 +13,8 @@ public class Project extends bee.api.Project {
 
     {
         product("com.github.teletha", "stylist", ref("version.txt"));
+
+        require(SourceVersion.RELEASE_16, SourceVersion.RELEASE_11);
 
         require("com.github.teletha", "sinobu");
         require("com.github.teletha", "antibug").atTest();
